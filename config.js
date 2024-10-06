721b75c736e6e279ee235634241fe4dba641ab8d
HansaTech77 <hansatech77@gmail.com>
2024-10-06 08:04:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "TIESmSBZ#5RKtbPLEFjfp--_UCPbnAsSAQcM26_pzIVqhV7UM4Zo",
+SESSION_ID: process.env.SESSION_ID || "1IxWyA5Q#0YlYGvHFkvFkkKGoGG3Ow1UaLkKGs5YRpoD2BcVxyfQ",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
