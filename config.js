f84356b7b8aa9597dcd780397f689dfa198232f4
SahiyaTech75 <sahiyatech@gmail.com>
2024-10-08 13:16:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "zOIUFSDZ#Ex5bQnZHzkhJFRLBoR13B7DASpVYGSxgYJ4Jx5-kwIs",
+SESSION_ID: process.env.SESSION_ID || "6Vt3SRZD#xbHkr91-aVkEWVm52Ntlcx0aP9MnkLORj6ogg-nG1og",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
